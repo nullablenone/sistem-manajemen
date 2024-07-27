aa2d03c1bc3135a01cfffc7a3abbdfea28b6e64b
useryesa9@gmail.com <useryesa9@gmail.com>
2024-07-27 20:22:10 +0700

    membuat model atau migrasi serta membuatkan relasi data base

---
+++ app/Models/ProdukModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProdukModel extends Model
+{
+    use HasFactory;
+
+    public function sepatuSendal(){
+        return $this->hasMany(SepatuSendal::class);
+    }
+
+}
---
+++ database/migrations/2024_07_27_034052_create_sepatu_sendals_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sepatu_sendals', function (Blueprint $table) {
+            $table->id();
+            $table->string('chesk');
+            $table->foreignId('model_id')->constrained('sepatu_sendals')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sepatu_sendals');
+    }
+};
