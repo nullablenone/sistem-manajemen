430ac38e21597e13a9e1e18df2f8581617f1842b
useryesa9@gmail.com <useryesa9@gmail.com>
2024-07-28 11:49:05 +0700

    fix urutan migrasi

---
+++ database/migrations/2024_07_28_044317_sepatu_sendals_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sepatu_sendals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('models_id')->constrained('produk_models')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
