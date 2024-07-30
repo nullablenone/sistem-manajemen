527bd7a4d4ded15730b35d8ab11335507d2b4601
useryesa9@gmail.com <useryesa9@gmail.com>
2024-07-30 13:43:25 +0700

    membuat halaman dashboard berdasarkan controller

--- resources/views/dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts')
-@section('title', 'Dashboard')
-@section('title-heading', 'Dashboard')
-    
-@section('content')
-<h1>Test</h1>   
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SepatuSendalController;
 use Illuminate\Support\Facades\Route;
@@ -9,9 +10,7 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
     Route::get('sepatu-sendal', [SepatuSendalController::class, 'index'])->name('sepatuSendal.index');
     Route::get('sepatu-sendal/create', [SepatuSendalController::class, 'create'])->name('sepatuSendal.create');
