1cf7d18a96225d9774818220030bfe34e8a1f592
useryesa9@gmail.com <useryesa9@gmail.com>
2024-07-31 15:24:00 +0700

    update halaman create

--- resources/views/SepatuDanSendal/create.blade.php
+++ resources/views/SepatuDanSendal/create.blade.php
@@ -1,39 +1,48 @@
 @extends('layouts')
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <div class="card-title">Form Elements</div>
-                </div>
-                <div class="card-body">
-                    <div class="row">
+    <form action="{{ route('sepatuSendal.store') }}" method="POST">
+        @csrf
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-title">Tambah Produk</div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md">
 
-                        {{-- <div class="col-md-6 col-lg-4"> --}}
-                        <div class="col-md">
-                            {{-- <label class="mb-3"><b>Form Group Default</b></label>
-                            <div class="form-group form-group-default">
-                                <label>Input</label>
-                                <input id="Name" type="text" class="form-control" placeholder="Fill Name">
-                            </div> --}}
-                            <div class="col-md-6 col-lg-4">
-                                <div class="form-group form-group-default">
-                                    <label>Select</label>
-                                    <select class="form-select" id="formGroupDefaultSelect" name="produk_model">
-                                        @foreach ($models as $model)
-                                            <option value="{{ $model->id }}">{{ $model->nama }}</option>
-                                        @endforeach
-                                    </select>
+                                <div class="col-md-6 col-lg-4">
+                                    <div class="form-group form-group-default">
+                                        <label>Model</label>
+                                        <select class="form-select" id="formGroupDefaultSelect" name="produk_model">
+                                            @foreach ($models as $model)
+                                                <option value="{{ $model->id }}">{{ $model->nama }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
                                 </div>
+                                
+                                <div class="col-md-6 col-lg-4">
+                                    <div class="form-group form-group-default">
+                                        <label>Ukuran</label>
+                                        <select class="form-select" id="formGroupDefaultSelect" name="ukuran">
+                                            @foreach ($ukurans as $ukuran)
+                                                <option value="{{ $ukuran->id }}">{{ $ukuran->ukuran }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="card-action">
-                    <button class="btn btn-success">Submit</button>
-                    <button class="btn btn-danger">Cancel</button>
+                    <div class="card-action">
+                        <button class="btn btn-success">Submit</button>
+                        <button class="btn btn-danger">Cancel</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </form>
 @endsection
