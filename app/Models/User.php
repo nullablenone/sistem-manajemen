ad5898111e16c8bc5bb7c4cbd7dab10318edacfe
useryesa9@gmail.com <useryesa9@gmail.com>
2024-08-15 21:20:41 +0700

    membuat role super admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'role',
         'password',
     ];
 
@@ -43,5 +44,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function hasRole($role)
+    {
+        return $this->role === $role;
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,8 +14,16 @@ class UsersTableSeeder extends Seeder
     public function run(): void
     {
         User::create([
+            'name' => 'super admin',
+            'email' => 'super@admin.xxx',
+            'role' => 'super admin',
+            'password' => bcrypt('super@admin.xxx'),
+        ]);
+
+        User::create([
             'name' => 'Admin',
             'email' => 'admin@admin.xxx',
+            'role' => 'admin',
             'password' => bcrypt('admin@admin.xxx'),
         ]);
     }
